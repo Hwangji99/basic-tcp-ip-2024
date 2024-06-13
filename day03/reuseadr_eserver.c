b2030ea6dd0e1efd0cf3fb835432df5ef6b9168b
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-13 15:26:37 +0900

    Update reuseadr_eserver.c

--- day03/reuseadr_eserver.c
+++ day03/reuseadr_eserver.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
   serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
   serv_adr.sin_port=htons(atoi(argv[1]));
 
-  if(bind(serv_sock, (struct sockaddr*)&serv)adr,sizeof(serv_adr)))
+  if(bind(serv_sock, (struct sockaddr*)&serv_adr,sizeof(serv_adr)))
     error_handling("blind() error");
   if(listen(serv_sock, 5)==-1)
     error_handling("listen error");
