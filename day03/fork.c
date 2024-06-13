873c3ef37d28feef9514009c64954bdc13d75b0a
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-13 16:23:54 +0900

    Update fork.c

--- day03/fork.c
+++ day03/fork.c
@@ -8,11 +8,11 @@ int main(int argc, char *argv[])
   int lval=20;
   gval++, lval+=5;
 
-  pid=fork();
-  if(pid==0) // if Child Process
+  pid=fork();  // 자식 프로세스 생성. 따라서 부모 프로세스의 pid에는 자식 프로세스의 ID가 저장, 자식 pid에는 0이 저
+  if(pid==0) // if Child Process  // 12,18행 : 자식 프로세스는 이 두 문장을 실행. pid에 0이 저장되기 때문
     gval+=2, lval+=2;
   else        // if Parent Process
-    gval-=2, lval-=2;
+    gval-=2, lval-=2;  // 15, 20행: 부모 프로세스는 이 두 문장을 실행. pid에 자식 프로세스의 ID가 저장되기 때문
 
   if(pid==0)
     printf("Child Proc: [%d, %d] \n", gval, lval);
