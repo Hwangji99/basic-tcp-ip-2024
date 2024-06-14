d9ec9cadc7fb272e0a4ac0b61d7265aafe9e2001
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-14 15:09:38 +0900

    Update pipe3.c

--- day04/pipe3.c
+++ day04/pipe3.c
@@ -10,12 +10,12 @@ int main(int argc, char *argv[])
   char buf[BUF_SIZE];
   pid_t pid;
 
-  pipe(fds1), pipe(fds2);  // pipe 함수호출을 통해 파이프 생성. 이로 인해 fds에는 압출력을 위한 파일 디스크립터가 각각 저장
-  pid=fork();  // 이어서 fork 함수호출 12행으로 얻은 2 개의 디스크립터를 소유
+  pipe(fds1), pipe(fds2);  // 두 개의 파이프 생성
+  pid=fork();  
   if(pid==0)
   {
-    write(fds1[1], str1, sizeof(str1));
-    read(fds2[0], buf, BUF_SIZE);
+    write(fds1[1], str1, sizeof(str1));  // 17,23행 : 자식 프로세스에서 부모 프로세스로의 데이터 전송은 배열 fds1이 참조하는 파이프를 통해 이뤄짐
+    read(fds2[0], buf, BUF_SIZE);  // 18,25행 : 부모 프로세스에서 자식 프로세스로의 데이터 전송은 배열 fds1이 참조하는 파이프를 통해 이뤄짐
     printf("Child proc output: %s \n", buf);
   }                                   
   else
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     read(fds1[0], buf, BUF_SIZE);
     printf("Parent proc output: %s \n", buf);
     write(fds2[1], str2, sizeof(str2));
-    sleep(3);
+    sleep(3);  // 큰 의미는 없다. 다만 부모 프로세스의 종료를 지연시키기 위해 삽입
   }
   return 0;
 }
