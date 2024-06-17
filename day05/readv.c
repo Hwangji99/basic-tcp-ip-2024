63cc8c817f3333dbb7368e6c75fb4624e46fbf57
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-17 11:47:13 +0900

    Create readv.c

---
+++ day05/readv.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/uio.h>
+#define BUF_SIZE 100
+
+int main(int argc, char *argv[])
+{
+  struct ivoec vec[2];
+  char buf1[BUF_SIZE]={0,};
+  char buf2[BUF_SIZE]={0,};
+  int str_len;
+
+  vec[0].iov_base=buf1;  // 12, 13행 : 첫 번째 데이터 저장소의 위치와 저장할 데이터의 크기정보를 설정
+  vec[0].iov_len=5;      // 특히 저장할 데이터의 크기를 5로 지정했기 때문에 buf1의 크기에 상관없이 최대 5바이트만이 영역에 저장됨 
+  vec[1].iov_base=buf2;    // 14, 15행 : vec[0]에 등록되어 있는 버퍼에 5바읕가 저장되고, 나머지 데이터는 vec[1]에 등록되어있는 버퍼에 저장
+  vec[1].iov_len=BUF_SIZE; // 특히 구조체 iovec의 멤버 iov_len에는 버퍼에 저장할 최대 바이트 크기 정보를 저장해야함 
+
+  str_len=readv(0, vec, 2);  // readv 함수의 첫 번째 전달인자가 0이기 때문에 콘솔로부터 데이터를 수신함
+  printf("Read bytes: &d \n", str_len);
+  printf("First message : &d \n", buf1);
+  printf("Second message : &d \n", buf2);
+  return 0;
+}
