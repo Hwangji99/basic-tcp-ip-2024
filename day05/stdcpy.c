c526cee9d840769621d0240c12a4fafce3fb3f4b
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-17 13:55:36 +0900

    Update stdcpy.c

--- day05/stdcpy.c
+++ day05/stdcpy.c
@@ -7,13 +7,13 @@ int main(int argc, char *argv[])
   FILE * fp2;  // fp2에 저장되는 것도 FILE 구조체의 포인터
   char buf[BUF_SIZE];
 
-  fp1=open("news.txt", "r");
-  fp2=open("cpy.txt", "w");
+  fp1=fopen("news.txt", "r");
+  fp2=fopen("cpy.txt", "w");
 
   while(fgets(fp1, BUF_SIZE, fp1)!=NULL)
     fputs(buf, fp2);
 
-  close(fp1);
-  close(fp2);
+  fclose(fp1);
+  fclose(fp2);
   return 0;
 }
