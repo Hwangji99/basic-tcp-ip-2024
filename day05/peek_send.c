e5aea5a2efef7194204a090c10abbc5ecb0acc54
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-17 11:20:10 +0900

    Update peek_send.c

--- day05/peek_send.c
+++ day05/peek_send.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
   }
 
   sock=socket(PF_INET, SOCK_STREAM, 0);
-  memset(&send_adr, 0, sizeof(recv_adr));
+  memset(&send_adr, 0, sizeof(send_adr));
   send_adr.sin_family=AF_INET;
   send_adr.sin_addr.s_addr=inet_addr(argv[1]);
   send_adr.sin_port=htons(atoi(argv[2]));
