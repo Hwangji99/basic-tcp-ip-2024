da5c9cf97fa06538304b88bf4c8eac5c426fe42b
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-17 11:48:52 +0900

    Update writev.c

--- day05/writev.c
+++ day05/writev.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
   vec[0].iov_base=buf1;  // 11, 12행 : 첫 번째로 전송할 데이터가 지정된 위치와 크기정보를 담고 있다
   vec[0].iov_len=3;
   vec[1].iov_base=buf2;  // 13, 14행 : 두 번째로 전송할 데이터가 지정된 위치와 크기정보를 담고 있다
-  vec[1].iov_base=4;
+  vec[1].iov_len=4;
 
   str_len=writev(1, vec, 2);  // writev 함수의 첫 번째 전달인자가 1이므로 콘솔로 출력이 이루어짐
   puts("");
