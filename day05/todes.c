b5526c5e3ca0b548519c1c5a08555e109149c1c8
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-17 15:09:09 +0900

    Update todes.c

--- day05/todes.c
+++ day05/todes.c
@@ -14,7 +14,7 @@ int main(void)
   printf("First file descriptor: %d \n", fd);  // 7행에서 반환된 파일 디스크립터의 정수 값을 출력하고 있다
   fp=fdopen(fd, "w");    // 15행에서는 fdopen 함수호출을 통해 파일 디스크립터를 FILE 포인터로
   fputs("TCP/IP SOCKET PROGRAMMING \n", fp); 
-  printf("Second file descriptor: %d \n", fileno(fd));   // 17행에서는 fileno 함수호출을 통해 이를 다시 파일 디스크립터로 변환,  그 정수 값을 출
+  printf("Second file descriptor: %d \n", fileno(fp));   // 17행에서는 fileno 함수호출을 통해 이를 다시 파일 디스크립터로 변환,  그 정수 값을 출
   fclose(fp);
   return 0;
 }
