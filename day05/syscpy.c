6fde0ea8dd532f096982153c2748dd76e77a8c2d
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-17 13:30:50 +0900

    Update syscpy.c

--- day05/syscpy.c
+++ day05/syscpy.c
@@ -8,10 +8,10 @@ int main(int argc, char *argv[])
   int len;
   char buf[BUF_SIZE];
 
-  fd1=open("news.txt", O-RDONLY);
+  fd1=open("news.txt", O_RDONLY);
   fd2=open("cpy.txt", O_WRONLY|O_CREAT|O_TRUNC);
 
-  while(len=read(fd1, buf, sizeof(buf)))>0)
+  while((len=read(fd1, buf, sizeof(buf)))>0)
     write(fd2, buf, len);
 
   close(fd1);
